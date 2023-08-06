8575686f8e9efea5211a812f52c4a3bc822ca3a8
James Wilcock <jameswilcock8@gmail.com>
2023-08-06 16:30:43 -0700

    good margin oh yeah

--- Chat.gd
+++ Chat.gd
@@ -3,11 +3,14 @@ var chat_id = "0"
 @export var chat_label: Label
 @export var name_label: Label
 
-func set_chat_text(text):
-	chat_label.text = text
+func set_size_for_wrap():
 	var ysize = chat_label.get_line_count() * chat_label.get_line_height()
 	print(ysize)
 	set_custom_minimum_size(Vector2(0,ysize))
+
+func set_chat_text(text):
+	chat_label.text = text
+	set_size_for_wrap()
 
 	
 func set_chat_id(id):
@@ -15,3 +18,7 @@ func set_chat_id(id):
 
 func set_name_text(name):
 	name_label.text = name + ": "
+
+
+func _on_chat_label_resized():
+	set_size_for_wrap()
