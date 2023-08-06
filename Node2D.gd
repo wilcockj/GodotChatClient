9172d28992ce61033b9695b9f651f7333b1addae
James Wilcock <jameswilcock8@gmail.com>
2023-08-06 14:44:06 -0700

    got sending working

--- Node2D.gd
+++ Node2D.gd
@@ -1,15 +1,24 @@
 extends Node2D
 
+@onready var text_input = $CanvasLayer/Control/HBoxContainer/MarginContainer2/LineEdit
 # The URL we will connect to
 @export var websocket_url = "wss://swiftnotes.net/ws"
 
+var uuid_util = preload('res://uuid.gd').new()
 
-var chat = {"finished": false, "message": "", "userid": "", "username": "", "uuid":""}
+# make chat a class with methods to send and set timestamp etc.
+var chat = {"finished": false, "message": "", "userid": "", "username": "", "uuid":"", "timestamp": 0}
 # Our WebSocketClient instance
 var socket = WebSocketPeer.new()
 
 func _ready():
+	print(uuid_util.generate_uuid())
 	socket.connect_to_url(websocket_url)
+	chat.userid = uuid_util.generate_uuid()
+	chat.username = "JohnGodot"
+	chat.finished = false
+	chat.timestamp = Time.get_unix_time_from_system()
+	chat.uuid = uuid_util.generate_uuid()
 
 func _process(delta):
 	socket.poll()
@@ -32,11 +41,27 @@ func _process(delta):
 		var reason = socket.get_close_reason()
 		print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
 		set_process(false) # Stop processing.
+		# need to add actual retry connecting with backoff
 		socket.connect_to_url(websocket_url)
 
-
-func _on_text_edit_text_changed():
+func _on_line_edit_text_changed(new_text):
 	var state = socket.get_ready_state()
 	if state == WebSocketPeer.STATE_OPEN:
 		# send the json object stringified
-		pass
+		print(new_text)
+		chat.message = new_text
+		var json = JSON.new()
+		socket.send_text(json.stringify(chat))
+
+
+
+func _on_line_edit_text_submitted(new_text):
+	print("submitted this trash: ", new_text)
+	text_input.clear()
+	chat.finished = true
+	chat.message = new_text
+	var state = socket.get_ready_state()
+	if state == WebSocketPeer.STATE_OPEN:
+		var json = JSON.new()
+		socket.send_text(json.stringify(chat))
+		chat.uuid = uuid_util.generate_uuid()
