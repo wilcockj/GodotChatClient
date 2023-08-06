5ebe05d6d8549ed39e966a7edfc1307cad478ea4
James Wilcock <jameswilcock8@gmail.com>
2023-08-06 16:26:13 -0700

    bodge to move on line wrap

--- Chat.gd
+++ Chat.gd
@@ -5,6 +5,10 @@ var chat_id = "0"
 
 func set_chat_text(text):
 	chat_label.text = text
+	var ysize = chat_label.get_line_count() * chat_label.get_line_height()
+	print(ysize)
+	set_custom_minimum_size(Vector2(0,ysize))
+
 	
 func set_chat_id(id):
 	chat_id = id
